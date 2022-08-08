631299f037da89b94d86b163d6beae3f7feb9d56
minhlq-uit <qminh0410@gmail.com>
2022-08-08 10:36:39 +0700

    feat: modal

--- components/footer/footer.vue
+++ components/footer/footer.vue
@@ -114,7 +114,7 @@
                 target="_blank"
                 class="cursor-pointer hover:bg-red"
               >
-                <!-- <svg
+                <svg
                   class="w-8 h-8 ml-auto mr-5"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 512 512"
@@ -122,8 +122,8 @@
                 >
                   <path
                     d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"
-                  /></svg
-              > -->
+                  />
+                </svg>
               </a>
             </div>
             <div class="col-end-4">
@@ -132,7 +132,7 @@
                 target="_blank"
                 class="cursor-pointer hover:bg-red"
               >
-                <!-- <svg
+                <svg
                   class="w-8 h-8 ml-auto"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 448 512"
@@ -141,56 +141,55 @@
                   <path
                     d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
                   />
-                </svg> -->
+                </svg>
               </a>
             </div>
           </div>
           <div class="languages h-8">
-            <!-- <el-dropdown>
-              <div class="languages-logo mr-6">
-                <span class="el-dropdown-link grid grid-cols-3">
+            <!-- start dropdown -->
+            <div>
+              <div class="relative">
+                <!-- Dropdown toggle button -->
+                <button
+                  @click="isOpen"
+                  class="text-right p-2 pr-4 text-black rounded-md w-full"
+                >
+                  <span class="mr-4">{{ isLocale }}</span>
                   <svg
+                    class="w-5 h-5 dark:text-white inline-block text-black"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="currentColor"
                   >
                     <path
                       fill-rule="evenodd"
-                      d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z"
+                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                       clip-rule="evenodd"
                     />
                   </svg>
-                  <p
-                    class="col-span-2 self-center ml-2 text-lg"
-                    v-if="isLocale === ''"
-                  >
-                    Languages
-                  </p>
-                  <p
-                    class="col-span-2 self-center ml-2 text-lg lg:pl-8 xl:pl-14"
-                    v-if="isLocale === 'en'"
+                </button>
+
+                <!-- Dropdown menu -->
+                <div
+                  v-show="show"
+                  class="absolute right-0 -top-[60px] py-2 mt-2 bg-gray-300 rounded-md shadow-xl w-44"
+                >
+                  <span
+                    class="block px-4 py-2 text-sm text-black hover:bg-indigo-400 hover:text-black"
+                    @click="changeLocale('en')"
                   >
                     English
-                  </p>
-                  <p
-                    class="col-span-2 self-center ml-2 text-lg lg:ml-1 xl:pl-5"
-                    v-if="isLocale === 'vi'"
+                  </span>
+                  <span
+                    class="block px-4 py-2 text-sm text-black hover:bg-indigo-400 hover:text-black"
+                    @click="changeLocale('vi')"
                   >
                     Vietnamese
-                  </p>
-                </span>
+                  </span>
+                </div>
               </div>
-              <template #dropdown>
-                <el-dropdown-menu>
-                  <el-dropdown-item @click="changeLocale('en')"
-                    >English</el-dropdown-item
-                  >
-                  <el-dropdown-item @click="changeLocale('vi')"
-                    >Vietnamese</el-dropdown-item
-                  >
-                </el-dropdown-menu>
-              </template>
-            </el-dropdown> -->
+            </div>
+            <!-- end dropdown-->
           </div>
         </div>
       </div>
@@ -208,16 +207,22 @@
 export default {
   name: "Footer",
   data() {
-    return {};
+    return {
+      show: false,
+    };
   },
   methods: {
     changeLocale(value) {
       this.$i18n.locale = value;
+      this.show = !this.show;
+    },
+    isOpen() {
+      this.show = !this.show;
     },
   },
   computed: {
     isLocale() {
-      return this.$i18n.locale;
+      return this.$i18n.locale === "en" ? "English" : "Vietnamese";
     },
   },
 };
